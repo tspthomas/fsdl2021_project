593c5d0dc31778f40459f07f6fc88662265883d7
Thomas Paula <tsp.thomas@gmail.com>
2021-04-05 20:41:13 -0300

    Adding updated airflow version

---
+++ airflow_new/build_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t airflow_server_new -f Dockerfile ../
--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mlflow ui \
+mlflow server \
     --host 0.0.0.0 \
     --port $MLFLOW_SERVER_PORT \
     --default-artifact-root $MLFLOW_ARTIFACT_ROOT
